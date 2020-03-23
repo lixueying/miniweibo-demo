05258a1e7b5e58b3dbbca18b3222bbbacb18da7a
xueying <xyli@thoughtworks.com>
2020-03-23 23:11:21 +0800

    Initial Commit

---
+++ miniweibo-demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  miniweibo-demo
+//
+//  Created by xyli on 2020/3/23.
+//  Copyright © 2020 xyli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
